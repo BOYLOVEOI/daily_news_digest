14db0a9460af87bbc1f19a5673c5e951c378a04b
Paul Lavoie <plavoie@gmu.edu>
2024-05-23 20:50:55 -0400

    Emailing news title, description, and link

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Import Statement
-import smtplib
+import smtplib, ssl
 import os
 import requests
+from send_email import send_email
 
 # Storing the news API key
 api_key = "82d97b1654a94571b989e207c0362d52"
@@ -16,8 +17,20 @@ request = requests.get(url)
 # Storing the response in content as a JSON
 content = request.json()
 
+# Creating the message string
+body = "Subject: Today's News\n"
 
 # Retrieving only the titles and descriptions of each articles stored in the 
 # JSON response
 for i in content["articles"]:
-       print(i["title"], ": ", i["description"])
+       # If the article title is NOT None (article exists)..
+       if i["title"] is not None:
+              # appending the article title, description, and link to the message str
+              body = body + "Title: " + i["title"] + "\n" + "Description: " + \
+                     i["description"] + "\n" + "Link: " + i["url"] + 2*"\n"
+
+# Encoding the body with the utf-8 format in case of potential encoding errors
+body = body.encode("utf-8")
+
+# Sending the message to the user
+send_email(body)
